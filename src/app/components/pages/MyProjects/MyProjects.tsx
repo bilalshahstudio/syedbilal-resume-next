b3a61db57c05bb3df5d0d06475e0c4b929a8ee40
bilalshahcgpt <bilalshahstudio@gmail.com>
2026-04-09 19:00:18 +0500

    weather forecast app link updated

--- src/app/components/pages/MyProjects/MyProjects.tsx
+++ src/app/components/pages/MyProjects/MyProjects.tsx
@@ -37,6 +37,12 @@ export default function MyProjects() {
       projectURL: "https://budget-tracking-app-fe.vercel.app/",
     },
     {
+      title: "Weather Forecasting App",
+      imgSrc:
+        "https://images.unsplash.com/photo-1630260667842-830a17d12ec9?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      projectURL: "https://weather-app-bilal.vercel.app/",
+    },
+    {
       title: "E-commerce Store",
       imgSrc:
         "https://images.unsplash.com/photo-1441986300917-64674bd600d8?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
